f2d1ffd15899d304252ed2beed390854dd85bb9c
a <a@a.a>
2022-04-12 21:02:57 -0400

    refactoring

--- commands.py
+++ commands.py
@@ -1,9 +1,13 @@
 import keyboard
-import other_keyboard
 from uuid import UUID
 import asyncio
-import winsound
 import time
+
+try: #these 2 only work on windows
+    import other_keyboard
+    import winsound
+except:
+    pass
 
 actionQueue = asyncio.Queue()
 def q(action):
@@ -12,66 +16,37 @@ def q(action):
 def sound(filename):
     winsound.PlaySound(filename, winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_NODEFAULT)
 
-# queue up your custom actions in these callbacks with the q function, like q(myAction)
-# refer to the Twitch/Streamelements documentation for more details on the available data in each callback
-
-def streamelementsTipCallback(event: dict) -> None:
-    data = event["data"]
-    print("%s tipped %.2f %s" % (data["username"], data["amount"], data["currency"]))
-    q(mediumHeadpat)
-
-def streamlabsTipCallback(event: dict) -> None:
-    data = event["message"][0]
-    print("%s tipped %s" % (data["from"], data["formatted_amount"]))
-    q(mediumHeadpat)
-
-def bitCallback(uuid: UUID, response: dict) -> None:
-    data = response["data"]
-    print("got %d bits from %s" % (data["bits_used"], data["user_name"]))
-    if data["bits_used"] >= 1000:
-        q(longHeadpat)
-    elif data["bits_used"] >= 200:
-        q(mediumHeadpat)
-    else:
-        q(shortHeadpat)
-
-def subCallback(uuid: UUID, response: dict) -> None:
-    data = response["data"]
-    if "display_name" in data["message"]:
-        print("sub from %s" % (data["message"]["display_name"]))
-    else:
-        print("unknown sub")
-    q(mediumHeadpat)
-
-def pointCallback(uuid: UUID, response: dict) -> None:
-    data = response["data"]
-    print("%s redeemed %s" % (data["redemption"]["user"]["display_name"], data["redemption"]["reward"]["title"]))
-    # adjust the title here to match the one set in twitch
-    if data["redemption"]["reward"]["title"] == "Headpat":
-        q(mediumHeadpat)
-
-pomfUserTimes = {}
-pomfGlobalTime = 0
-def pomfCallback(user: str, message: str) -> None:
-    global pomfUserTimes
-    global pomfGlobalTime
+userTimes = {}
+globalTime = 0
+def messageCallback(user: str, message: str) -> None:
+    global userTimes
+    global globalTime
     t = time.time()
-    if t - pomfGlobalTime < 0: # shared cooldown, only do 1 command every this many seconds, change to 0 to disable
+
+    # shared cooldown, only do 1 command every this many seconds, change to 0 to disable
+    if t - globalTime < 5: 
         return
-    if user in pomfUserTimes and t - pomfUserTimes[user] < 60: # per user cooldown, change to 0 to disable
+
+    # per user cooldown, change to 0 to disable
+    if user in userTimes and t - userTimes[user] < 60: 
         return
-    # add more commands here, make sure to copy the `pomfGlobalTime = t` and `pomfUserTimes[user] = t` lines if you want the cooldown to work
+
+    # Add more commands here
+    # copy the `globalTime = t` and `userTimes[user] = t` lines if you want the cooldown to work
+    # queue up your custom actions in these callbacks with the q function, like q(myAction)
+    # for actions that take arguments, do it like q(lambda: myAction(arg))
     if message == "!headpat":
-        pomfGlobalTime = t
-        pomfUserTimes[user] = t
+        globalTime = t
+        userTimes[user] = t
         q(shortHeadpat)
-    elif message == "!headpaat":
-        pomfGlobalTime = t
-        pomfUserTimes[user] = t
-        q(mediumHeadpat)
+    elif message == "!headpat-twice":
+        globalTime = t
+        userTimes[user] = t
+        q(shortHeadpat)
+        q(shortHeadpat)
     elif message == "!headpaaat":
-        pomfGlobalTime = t
-        pomfUserTimes[user] = t
+        globalTime = t
+        userTimes[user] = t
         q(longHeadpat)
 
 # define your custom actions here
@@ -80,13 +55,8 @@ def pomfCallback(user: str, message: str) -> None:
 
 def shortHeadpat():
     sound("short sound.wav")
-    keyboard.press_and_release('ctrl+a')
+    keyboard.press_and_release('shift+a')
     return 1
-
-def mediumHeadpat():
-    sound("medium sound.wav")
-    other_keyboard.PressAndRelease('b')
-    return 2
 
 def longHeadpat():
     sound("long sound.wav")
--- headpattr.py
+++ headpattr.py
@@ -1,22 +1,13 @@
-from twitchAPI.pubsub import PubSub
-from twitchAPI.twitch import Twitch
-from twitchAPI.types import AuthScope
 from pprint import pprint
 from uuid import UUID
 import asyncio
 
-import streamelementsIntegration
-import streamlabsIntegration
 import pomfIntegration
+##import twitchIntegration
 import commands
 
 #read config
 twitchusername = ""
-accesstoken = ""
-refreshtoken = ""
-clientid = ""
-streamelementsjwt = ""
-streamlabssocket = ""
 pomfusername = ""
 with open("apikeys.txt") as f:
     content = f.readlines()
@@ -24,48 +15,16 @@ for line in content:
     line = line.strip()
     if line.startswith("twitchusername"):
         twitchusername = line[len("twitchusername:"):]
-    elif line.startswith("accesstoken"):
-        accesstoken = line[len("accesstoken:"):]
-    elif line.startswith("refreshtoken"):
-        refreshtoken = line[len("refreshtoken:"):]
-    elif line.startswith("clientid"):
-        clientid = line[len("clientid:"):]
-    elif line.startswith("streamelementsjwt"):
-        streamelementsjwt = line[len("streamelementsjwt:"):]
-    elif line.startswith("streamlabssocket"):
-        streamlabssocket = line[len("streamlabssocket:"):]
     elif line.startswith("pomfusername"):
         pomfusername = line[len("pomfusername:"):]
 
-# setting up Authentication and getting your user id
-if twitchusername and accesstoken and refreshtoken and clientid:
-    print("connecting to twitch...")
-    twitch = Twitch(clientid, authenticate_app=False)
-    twitch.set_user_authentication(accesstoken, [AuthScope.BITS_READ, AuthScope.CHANNEL_READ_SUBSCRIPTIONS, AuthScope.CHANNEL_READ_REDEMPTIONS], refreshtoken)
-    user_id = twitch.get_users(logins=[twitchusername])['data'][0]['id']
-
-    # starting up PubSub
-    pubsub = PubSub(twitch)
-    pubsub.start()
-    pubsub.listen_channel_points(user_id, commands.pointCallback)
-    pubsub.listen_channel_subscriptions(user_id, commands.subCallback)
-    pubsub.listen_bits(user_id, commands.bitCallback)
-    print("connected to twitch")
+if twitchusername:
+    twitchIntegration.listenForEvents(twitchusername, commands.messageCallback)
 else:
-    print("skipping twitch, at least one of [twitchusername accesstoken refreshtoken clientid] was blank")
-
-if streamelementsjwt:
-    streamelementsIntegration.listenForEvents(streamelementsjwt, commands.streamelementsTipCallback)
-else:
-    print("skipping streamelements, streamelementsjwt was blank")
-
-if streamlabssocket:
-    streamlabsIntegration.listenForEvents(streamlabssocket, commands.streamlabsTipCallback)
-else:
-    print("skipping streamlabs, streamlabssocket was blank")
+    print("skipping twitch, twitchusername was blank")
 
 if pomfusername:
-    pomfIntegration.listenForEvents(pomfusername, commands.pomfCallback)
+    pomfIntegration.listenForEvents(pomfusername, commands.messageCallback)
 else:
     print("skipping pomf, pomfusername was blank")
 
@@ -80,6 +39,4 @@ asyncio.get_event_loop().create_task(doActions())
 
 asyncio.get_event_loop().run_forever()
 
-#pubsub.unlisten(uuid)
-pubsub.stop()
 
